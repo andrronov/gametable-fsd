1f2aac54f9864d0f36f15c2043f0c5cf8d7ef6c1
andrronov <andrew270504cska@gmail.com>
2025-02-09 17:39:13 +0500

    fix: types

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { useRoute } from "vue-router";
-import { layouts } from "./layouts";
+import { layouts, type Layout } from "./layouts";
 
 const route = useRoute();
+
+const routeLayout = computed(() => route.meta.layout as Layout)
 </script>
 
 <template>
-  <component :is="layouts[route.meta.layout]">
+  <component :is="layouts[routeLayout]">
     <router-view />
   </component>
 </template>
